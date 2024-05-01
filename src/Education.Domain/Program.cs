9c1536eea76aebb71007dfbfbca9892af82790c6
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-05-01 11:38:58 +0500

    add first files

---
+++ src/Education.Domain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Education.Domain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
