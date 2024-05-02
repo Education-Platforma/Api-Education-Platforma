d9d8e5f8810c1194c4d7195b1af96cb367e36b05
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-02 18:02:22 +0500

    added coursefeedbacks

---
+++ src/Education.Application/UseCases/CourseFeedbackCase/Handlers/CommandHandler/UpdateCourseFeedbackHandler.cs
@@ -0,0 +1,42 @@
+﻿using Education.Application.Abstractions;
+using Education.Application.UseCases.CourseFeedbackCase.Commands;
+using Education.Domain.Entities.DemoModels;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.UseCases.CourseFeedbackCase.Handlers.CommandHandler
+{
+    public class UpdateCourseFeedbackHandler : IRequestHandler<UpdateCourseFeedbackCommand, ResponseModel>
+    {
+        private readonly IEducationDbContext _context;
+
+        public UpdateCourseFeedbackHandler(IEducationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponseModel> Handle(UpdateCourseFeedbackCommand request, CancellationToken cancellationToken)
+        {
+            var courseFeedback = await _context.CourseFeedbacks.FirstOrDefaultAsync(x  => x.Id == request.Id);
+
+            if (courseFeedback == null)
+            {
+                return new ResponseModel { Message = "CourseFeedback not found", StatusCode = 404, IsSuccess = true };
+            }
+
+            courseFeedback.Message = request.Message;
+
+            _context.CourseFeedbacks.Update(courseFeedback);
+
+            await _context.SaveChangesAsync();
+
+            return new ResponseModel { Message = "CourseFeedback updated successfully", StatusCode = 200, IsSuccess = true };
+
+        }
+    }
+}
---
+++ src/Education.Application/UseCases/CourseFeedbackCase/Handlers/QueryHandler/GetAllFeedbacksQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Education.Application.Abstractions;
+using Education.Application.UseCases.CourseFeedbackCase.Queries;
+using Education.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.UseCases.CourseFeedbackCase.Handlers.QueryHandler
+{
+    public class GetAllFeedbacksQueryHandler : IRequestHandler<GetAllFeedbacksQuery, List<CourseFeedbackModel>>
+    {
+        private readonly IEducationDbContext _context;
+        public GetAllFeedbacksQueryHandler(IEducationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CourseFeedbackModel>> Handle(GetAllFeedbacksQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.CourseFeedbacks.ToListAsync();
+
+            if(result == null)
+            {
+                return null!;
+            }
+            
+
+            return result;
+
+
+        }
+    }
+}
