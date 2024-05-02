2a8b7a3166c42bfc2881aa7455fcdd1b8da93242
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-02 20:03:48 +0500

    added quiz model

---
+++ src/Education.Application/UseCases/QuizCase/Handlers/CommandHandler/DeleteQuizHandler.cs
@@ -0,0 +1,49 @@
+﻿using Education.Application.Abstractions;
+using Education.Application.UseCases.QuizCase.Commands;
+using Education.Domain.Entities.DemoModels;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.UseCases.QuizCase.Handlers.CommandHandler
+{
+    public class DeleteQuizHandler : IRequestHandler<DeleteQuizCommand, ResponseModel>
+    {
+        private readonly IEducationDbContext _context;
+
+        public DeleteQuizHandler(IEducationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponseModel> Handle(DeleteQuizCommand request, CancellationToken cancellationToken)
+        {
+            var quiz = await _context.Quizes.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if(quiz == null)
+            {
+                return new ResponseModel()
+                {
+                    Message = "Quiz not found",
+                    StatusCode = 404,
+                    IsSuccess = true
+                };
+            }
+
+            _context.Quizes.Remove(quiz);
+            await _context.SaveChangesAsync();
+
+            return new ResponseModel()
+            {
+                Message = "Quiz deleted",
+                StatusCode = 200,
+                IsSuccess = false
+            };
+
+        }
+    }
+}
---
+++ src/Education.Application/UseCases/QuizCase/Handlers/QueryHandler/GetAllQuizQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Education.Application.Abstractions;
+using Education.Application.UseCases.QuizCase.Queries;
+using Education.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.UseCases.QuizCase.Handlers.QueryHandler
+{
+    public class GetAllQuizQueryHandler : IRequestHandler<GetAllQuizQuery, List<QuizModel>>
+    {
+        private readonly IEducationDbContext _context;
+
+        public GetAllQuizQueryHandler(IEducationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<QuizModel>> Handle(GetAllQuizQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Quizes.ToListAsync();
+
+            if (result.Count == 0 || result == null)
+            {
+                return null!;
+
+            }
+
+            return result;
+        }
+    }
+}
