2c38d4dadf675364e699ce120466af0646bca526
Hayotilla Tursunboev <coder0227@gmail.com>
2024-05-02 08:32:21 +0300

    Video-Lesson-VideoFeedback models done

---
+++ src/Education.Domain/Entities/LessonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Domain.Entities
+{
+    public class LessonModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int ExpForWatching { get; set; }
+        public  Guid VideoModelId { get; set; } 
+        public virtual VideoModel VideoModel { get; set; }
+    }
+}
