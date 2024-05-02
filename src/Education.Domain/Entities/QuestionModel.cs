eaa3de71089963b2d065d7a132211d2196e1acd4
Hayotilla Tursunboev <coder0227@gmail.com>
2024-05-02 10:26:09 +0300

    Question model done

---
+++ src/Education.Domain/Entities/QuestionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Domain.Entities
+{
+    public class QuestionModel
+    {
+        public Guid Id { get; set; }
+        public string OptionA { get; set; }
+        public string OptionB { get; set; }
+        public string OptionC { get; set; }
+        public char CorrectOption { get; set; }
+        public int Exp {  get; set; }
+    }
+}
