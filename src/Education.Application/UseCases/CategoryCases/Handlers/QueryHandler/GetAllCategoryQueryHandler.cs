23282bd2d0cdd987ffec38e5ad3f0ddb5afcd5ce
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-02 14:53:27 +0500

    *CategoryCase added

---
+++ src/Education.Application/UseCases/CategoryCases/Handlers/QueryHandler/GetAllCategoryQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Education.Application.Abstractions;
+using Education.Application.UseCases.CategoryCases.Queries;
+using Education.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.UseCases.CategoryCases.Handlers.QueryHandler
+{
+    public class GetAllCategoryQueryHandler : IRequestHandler<GetAllCategoryQuery, List<CategoryModel>>
+    {
+        private readonly IEducationDbContext _context;
+
+        public GetAllCategoryQueryHandler(IEducationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CategoryModel>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Categories.ToListAsync();
+
+            if(result is null)
+            {
+                throw new Exception("Category not found");
+            }
+
+            return result;
+        }
+    }
+}
