8a879acf5cfa750408d4a642a466ab2c7f93781a
Hayotilla Tursunboev <coder0227@gmail.com>
2024-05-05 14:12:04 +0300

    add

---
+++ src/Education.Infrastructure/Migrations/20240505102758_dsererervre.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Education.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class dsererervre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_CourseId",
+                table: "Groups");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_CourseId",
+                table: "Groups",
+                column: "CourseId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_CourseId",
+                table: "Groups");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_CourseId",
+                table: "Groups",
+                column: "CourseId");
+        }
+    }
+}
---
+++ src/Education.Infrastructure/Migrations/20240505110351_erwferf3.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Education.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class erwferf3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoPath",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoPath",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
