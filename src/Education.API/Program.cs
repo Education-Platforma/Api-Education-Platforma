e432409fd550acd40bb15aebcc0038065285c779
Hayotilla Tursunboev <coder0227@gmail.com>
2024-05-06 10:20:52 +0300

    Corrected some issues

--- src/Education.API/Program.cs
+++ src/Education.API/Program.cs
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseCors("_myAllowSpecificOrigins");
 
