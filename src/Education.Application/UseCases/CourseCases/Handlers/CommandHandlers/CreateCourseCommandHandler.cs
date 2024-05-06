3364dace1aa91a8d752ffa38b8f78a9a4f4a902b
Hayotilla Tursunboev <coder0227@gmail.com>
2024-05-06 11:47:35 +0300

    Group added

--- src/Education.Application/UseCases/CourseCases/Handlers/CommandHandlers/CreateCourseCommandHandler.cs
+++ src/Education.Application/UseCases/CourseCases/Handlers/CommandHandlers/CreateCourseCommandHandler.cs
@@ -1,13 +1,10 @@
 ﻿using Education.Application.Abstractions;
 using Education.Application.UseCases.CourseCases.Cammands;
+using Education.Application.UseCases.GroupCases.Commands;
 using Education.Domain.Entities;
 using Education.Domain.Entities.DemoModels;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Education.Application.UseCases.CourseCases.Handlers.CommandHandlers
@@ -15,10 +12,14 @@ namespace Education.Application.UseCases.CourseCases.Handlers.CommandHandlers
     public class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, ResponseModel>
     {
         private readonly IEducationDbContext _context;
-        public CreateCourseCommandHandler(IEducationDbContext context)
+        private readonly IMediator _mediator;
+
+        public CreateCourseCommandHandler(IEducationDbContext context, IMediator mediator)
         {
             _context = context;
+            _mediator = mediator;
         }
+
         public async Task<ResponseModel> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
         {
             var course = new CourseModel()
@@ -32,11 +33,31 @@ namespace Education.Application.UseCases.CourseCases.Handlers.CommandHandlers
                 CouponId = request.CouponId,
                 CategoryId = request.CategoryId,
             };
-             _context.Courses.Add(course);
+
+            _context.Courses.Add(course);
             await _context.SaveChangesAsync();
+
+            var groupCommand = new CreateGroupCommand()
+            {
+                GroupName = request.CourseName,
+                CourseId = course.Id 
+            };
+
+            var groupCreationResponse = await _mediator.Send(groupCommand);
+
+            if (!groupCreationResponse.IsSuccess)
+            {
+                return new ResponseModel()
+                {
+                    Message = "Failed to create group for the course",
+                    StatusCode = 500, 
+                    IsSuccess = false
+                };
+            }
+
             return new ResponseModel()
             {
-                Message = "Succesfully Created",
+                Message = "Successfully Created",
                 StatusCode = 201,
                 IsSuccess = true
             };
